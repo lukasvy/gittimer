b5a35b773c381b7f676c5af633e06ab9016ade48
Lukas Vyslocky <lukas.vyslocky@gmail.com>
2020-03-21 13:25:09 +0000

    wip on dynamic hor scroll

--- src/components/ActiveList.vue
+++ src/components/ActiveList.vue
@@ -17,26 +17,28 @@
                         {{activeBranch.getName()}}
                     </div>
                 </div>
-                <div class="item" style="float:right; font-size: 2em; font:bold;">
+                <div class="item" style="float:right; font-size: 70%; font:bold;">
                     {{activeBranch.getTimeSpent() ? activeBranch.getTimeSpent() : ''}}
                 </div>
             </div>
             <div class="ui attached middle aligned divided list huge">
-                <div class="item" v-for="branch in branchesList" :key="branch.getName()">
-                    <i class="ui icon code branch padded-icon"></i>
-                    <div class="content">
-                        <div class="header">{{branch.getName()}}</div>
-                        <template v-if="branch.getTimeSpent()">
-                            <small>
-                                {{branch.getTimeSpent() ? 'Time spent : '+ branch.getTimeSpent() : ''}}
-                            </small> |
-                            <small>
-                                {{branch.getLastAccess() ? 'Last access : '+ branch.getLastAccess() : ''}}
-                            </small>
-                        </template>
-                        <template v-if="!branch.getTimeSpent()">
-                            <small>no time recorder yet</small>
-                        </template>
+                <div class="scrollable-content">
+                    <div class="item" v-for="branch in branchesList" :key="branch.getName()">
+                        <i class="ui icon code branch padded-icon"></i>
+                        <div class="content">
+                            <div class="header text-overflow">{{branch.getName()}}</div>
+                            <template v-if="branch.getTimeSpent()">
+                                <small>
+                                    {{branch.getTimeSpent() ? 'Time spent : '+ branch.getTimeSpent() : ''}}
+                                </small> |
+                                <small>
+                                    {{branch.getLastAccess() ? 'Last access : '+ branch.getLastAccess() : ''}}
+                                </small>
+                            </template>
+                            <template v-if="!branch.getTimeSpent()">
+                                <small>no time recorder yet</small>
+                            </template>
+                        </div>
                     </div>
                 </div>
             </div>
@@ -49,8 +51,8 @@
     import {find} from 'underscore'
 
     export default {
-        name: "ActiveList",
-        data: function () {
+        name   : "ActiveList",
+        data   : function () {
             return {
                 repoList    : [],
                 activeRepo  : null,
@@ -75,5 +77,34 @@
 <style scoped>
     i.padded-icon {
         padding: 10px !important;
+    }
+
+    .text-overflow {
+        white-space: nowrap;
+        overflow: hidden;
+        text-overflow: ellipsis;
+    }
+
+    .scrollable-content {
+        overflow-y: scroll;
+        min-width: 200px;
+        height: 380px;
+    }
+
+    .scrollable-content {
+        overflow-y: scroll;
+        min-width: 200px;
+        height: 500px;
+    }
+    .item {
+        display: flex;
+        flex-direction: row;
+        padding-top: 15px;
+    }
+
+    .content {
+        padding-left: 20px;
+        min-width: 0px;
+        line-height: 1.2em;
     }
 </style>
